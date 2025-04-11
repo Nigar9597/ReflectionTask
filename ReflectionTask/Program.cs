5474beadb64d047dc171f2d9f8ae23eb427972e8
@Nigar9597 <@nigarza-bb208@code.edu.az>
2025-04-11 12:40:11 +0400

    Add project files.

---
+++ ReflectionTask/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+
+namespace ReflectionTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type userType = typeof(User);
+            object userInstance = Activator.CreateInstance(userType);
+
+           
+            FieldInfo idField = userType.GetField("id", BindingFlags.NonPublic | BindingFlags.Instance);
+            FieldInfo nameField = userType.GetField("name", BindingFlags.NonPublic | BindingFlags.Instance);
+            FieldInfo surnameField = userType.GetField("surname", BindingFlags.NonPublic | BindingFlags.Instance);
+            FieldInfo ageField = userType.GetField("age", BindingFlags.NonPublic | BindingFlags.Static);
+
+            idField.SetValue(userInstance, 1);
+            nameField.SetValue(userInstance, "Nigar");
+            surnameField.SetValue(userInstance, "Asadzade");
+            ageField.SetValue(null, 21); 
+
+            
+            Console.WriteLine("ID: " + idField.GetValue(userInstance));
+            Console.WriteLine("Name: " + nameField.GetValue(userInstance));
+            Console.WriteLine("Surname: " + surnameField.GetValue(userInstance));
+            Console.WriteLine("Age: " + ageField.GetValue(null));
+
+
+            MethodInfo getFullNameMethod = userType.GetMethod("GetFullName", BindingFlags.Public | BindingFlags.Instance);
+            getFullNameMethod.Invoke(userInstance, null);
+        }
+    }
+}
---
+++ ReflectionTask/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionTask
+{
+    class User
+    {
+        private int id;
+        private string name;
+        private string surname;
+        private static int age;
+
+        public void GetFullName()
+        {
+            Console.WriteLine($"{name} {surname}");
+        }
+
+        public static void ChangeAge(int newAge)
+        {
+            age = newAge;
+        }
+
+        public static int GetAge()
+        {
+            return age;
+        }
+    }
+}
